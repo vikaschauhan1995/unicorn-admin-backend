38a2827c460d61bca5bd1de0851d00c66de82178
Vikas Chauhan <vikas@rockstarchef.in>
2023-06-28 21:37:23 +0530

    createSubuser api added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,8 @@
 const User = require('../models/userModel');
 const jwt = require('jsonwebtoken');
-const { getPermissions } = require('./permissionController');
+const bcrypt = require('bcrypt');
+const { getPermissions, setPermission } = require('./permissionController');
+const { PASSWORD, USER_TYPE } = require('../models/userModel/const');
 
 const createToken = (_id) => {
   return jwt.sign({ _id }, process.env.JWT_SECRET_KEY, { expiresIn: 60 * 60 });
@@ -30,7 +32,33 @@ const signupUser = async (req, res) => {
   }
 }
 
+const createSubuser = async (req, res) => {
+  try {
+    const { email, password, permissions, user_type } = req.body;
+    if (!email || !password) {
+      throw Error('All fields must be filled')
+    }
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      throw Error('Email already in use');
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const user = await User.create({ email, [PASSWORD]: hash, [USER_TYPE]: user_type });
+
+    // create permissions
+    await setPermission(user._id, permissions);
+
+    // console.log("email, password, permissions=>", email, password, permissions);
+    res.status(200).json({});
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+}
+
 module.exports = {
   loginUser,
-  signupUser
+  signupUser,
+  createSubuser
 }
--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const { loginUser, signupUser } = require('../controllers/userController');
+const { loginUser, signupUser, createSubuser } = require('../controllers/userController');
 
 // login
 router.post('/login', loginUser);
@@ -10,5 +10,8 @@ router.post('/login', loginUser);
 
 // signup
 router.post('/signup', signupUser);
+
+// create subuser
+router.post('/subuser', createSubuser);
 
 module.exports = router;
