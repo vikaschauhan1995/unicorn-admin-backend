edd7b1fa477d66d25a60b60bff5a4a9a15d95344
Vikas Chauhan <vikas@rockstarchef.in>
2023-07-02 17:25:14 +0530

    crud api created for products

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,3 +1,4 @@
+const { default: mongoose } = require('mongoose');
 const Product = require('../models/productModel');
 const { PRODUCT_NAME, PRODUCT_SKU, PRODUCT_CREATED_BY_ID, PRODUCT_CREATED_BY_EMAIL, PRODUCT_MODIFIED_LAST } = require('../models/productModel/const.js');
 
@@ -5,8 +6,52 @@ const { PRODUCT_NAME, PRODUCT_SKU, PRODUCT_CREATED_BY_ID, PRODUCT_CREATED_BY_EMA
 const saveProduct = async (req, res) => {
   try {
     const { name, sku, created_by_id, created_by_email } = req.body;
+    const oldProduct = Product.findOne(sku);
+    if (oldProduct) {
+      throw Error('Duplicate SKU');
+    }
     const product = await Product.create({ [PRODUCT_NAME]: name, [PRODUCT_SKU]: sku, [PRODUCT_CREATED_BY_ID]: created_by_id, [PRODUCT_CREATED_BY_EMAIL]: created_by_email });
     // console.log("product=>", product);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+}
+
+const updateProduct = async (req, res) => {
+  try {
+    const { _id, name, sku } = req.body;
+    if (!mongoose.Types.ObjectId.isValid(_id)) {
+      return res.status(404).json({ error: 'Invalid id' });
+    }
+    const oldProduct = await Product.findOne({ _id });
+    if (!oldProduct) {
+      throw Error("Product id is invalid");
+    }
+    const newProductData = {
+      name,
+      sku,
+      [PRODUCT_MODIFIED_LAST]: new Date()
+    };
+    const newProduct = await Product.findOneAndUpdate({ _id }, {
+      ...newProductData
+    }, { new: true });
+    res.status(200).json(newProduct);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+}
+
+const deleteProduct = async (req, res) => {
+  const _id = req.params._id;
+  if (!mongoose.Types.ObjectId.isValid(_id)) {
+    res.status(404).json({ error: 'Invalid id' });
+  }
+  try {
+    const product = await Product.findOneAndDelete({ _id });
+    if (!product) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
     res.status(200).json(product);
   } catch (error) {
     res.status(400).json({ error: error.message });
@@ -25,5 +70,7 @@ const getProducts = async (req, res) => {
 
 module.exports = {
   saveProduct,
-  getProducts
+  getProducts,
+  updateProduct,
+  deleteProduct
 };
--- routes/product.js
+++ routes/product.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { saveProduct, getProducts } = require('../controllers/productController');
+const { saveProduct, getProducts, updateProduct, deleteProduct } = require('../controllers/productController');
 
 const router = express.Router();
 
@@ -7,5 +7,7 @@ const router = express.Router();
 // save product
 router.post('/', saveProduct);
 router.get('/', getProducts);
+router.patch('/', updateProduct);
+router.delete('/:_id', deleteProduct);
 
 module.exports = router;
