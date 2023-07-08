37529911976d5200acaad214006a6994e84e75e3
Vikas Chauhan <vikas@rockstarchef.in>
2023-07-08 15:31:38 +0530

    bugfix - orders item not showing in sidebar

--- models/permissionModel/const.js
+++ models/permissionModel/const.js
@@ -10,8 +10,8 @@ const PERMISSION_OBJ_COMPONENT_TO_BE_ACTIVE_KEY = 'componentToBeActive';
 const PERMISSION_OBJ_DESCRIPTION_KEY = 'description';
 
 // permissions
-const ORDERS_FULL_PERMISSION = { [PERMISSION_OBJ_KEY_KEY]: 'orders_full_permission', [PERMISSION_OBJ_COMPONENT_TO_BE_ACTIVE_KEY]: 'order', [PERMISSION_OBJ_DESCRIPTION_KEY]: 'Get full access of orders' };
-const ORDERS_READ_PERMISSION = { [PERMISSION_OBJ_KEY_KEY]: 'orders_read_permission', [PERMISSION_OBJ_COMPONENT_TO_BE_ACTIVE_KEY]: 'order', [PERMISSION_OBJ_DESCRIPTION_KEY]: 'Only see orders' };
+const ORDERS_FULL_PERMISSION = { [PERMISSION_OBJ_KEY_KEY]: 'orders_full_permission', [PERMISSION_OBJ_COMPONENT_TO_BE_ACTIVE_KEY]: 'orders', [PERMISSION_OBJ_DESCRIPTION_KEY]: 'Get full access of orders' };
+const ORDERS_READ_PERMISSION = { [PERMISSION_OBJ_KEY_KEY]: 'orders_read_permission', [PERMISSION_OBJ_COMPONENT_TO_BE_ACTIVE_KEY]: 'orders', [PERMISSION_OBJ_DESCRIPTION_KEY]: 'Only see orders' };
 const SUB_USER_FULL_ACCESS = { [PERMISSION_OBJ_KEY_KEY]: 'sub_user_full_access', [PERMISSION_OBJ_COMPONENT_TO_BE_ACTIVE_KEY]: 'subuser', [PERMISSION_OBJ_DESCRIPTION_KEY]: 'Get full access of sub user' };
 const SUB_USER_READ_ACCESS = { [PERMISSION_OBJ_KEY_KEY]: 'sub_user_read_access', [PERMISSION_OBJ_COMPONENT_TO_BE_ACTIVE_KEY]: 'subuser', [PERMISSION_OBJ_DESCRIPTION_KEY]: 'Only see sub users' }
 
