c14498bc383b56fa2e15188c9714a39fe15b11e4
Vikas Chauhan <vikas@rockstarchef.in>
2023-07-16 16:55:02 +0530

    inventory permission commented and add user-frontend on cors

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-  origin: `${process.env.FRONT_END_URL}`
+  origin: [`${process.env.FRONT_END_URL}`, `${process.env.USER_FRONT_END_URL}`]
 }));
 
 // routes
